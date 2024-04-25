90283296c0616c600e4291ec06033766163f0e17
Stijn_D'huys <stijndhuys@hotmail.com>
2024-04-25 17:28:59 +0800

    rename PaymentViewModel to PaymentWrapper

--- MauiApp1/StaticHelpers/ProjectsQuery.cs
+++ MauiApp1/StaticHelpers/ProjectsQuery.cs
@@ -13,9 +13,9 @@ static class ProjectsQuery
 
     }
 
-    public static IEnumerable<PaymentViewModel> GetProjectPaymentsWithinDates(IEnumerable<Project> projects, DateTime startDate, DateTime endDate)
+    public static IEnumerable<PaymentWrapper> GetProjectPaymentsWithinDates(IEnumerable<Project> projects, DateTime startDate, DateTime endDate)
     {
-        return projects.SelectMany(p => p.Payments).Where(x => x.Date >= startDate && x.Date <= endDate).Select(x => new PaymentViewModel(x));
+        return projects.SelectMany(p => p.Payments).Where(x => x.Date >= startDate && x.Date <= endDate).Select(x => new PaymentWrapper(x));
     }
 
     public static IEnumerable<ProjectExpense> GetProjectExpensesWithinDates(IEnumerable<Project> projects, DateTime startDate, DateTime endDate)
@@ -37,7 +37,7 @@ static class ProjectsQuery
         var currencyProjects = ProjectManager.AllProjects.Where(p => p.Currency == currency);
         var finishedProjects = GetFinishedProjects(currencyProjects);
 
-        var paymentVMs = currencyProjects.SelectMany(p => p.Payments).Where(x => x.Date.Month == date.Month && x.Date.Year == date.Year).Select(p => new PaymentViewModel(p));
+        var paymentVMs = currencyProjects.SelectMany(p => p.Payments).Where(x => x.Date.Month == date.Month && x.Date.Year == date.Year).Select(p => new PaymentWrapper(p));
 
         var absoluteExpenses = currencyProjects.SelectMany(p => p.Expenses).Where(x => !x.IsRelative && x.Date.Month == date.Month && x.Date.Year == date.Year).Sum(e => e.Amount);
         var relativeExpenses = finishedProjects.SelectMany(p => p.Expenses).Where(x => x.IsRelative && x.Date.Month == date.Month && x.Date.Year == date.Year).Sum(e => e.Amount);
