a769ffe2ddd55c9fbb905aaec761ba235ee28a13
Stijn_D'huys <stijndhuys@hotmail.com>
2024-09-24 14:32:22 +0800

    edit RelativeExpenseCalculator.cs
    take two separate expenses collections parameters, regular expenses and profitsharing expenses

--- MauiApp1/StaticHelpers/RelativeExpenseCalculator.cs
+++ MauiApp1/StaticHelpers/RelativeExpenseCalculator.cs
@@ -4,15 +4,15 @@ namespace MauiApp1.StaticHelpers;
 
 public static class RelativeExpenseCalculator
 {
-    public static void SetRelativeExpensesAmounts(IEnumerable<ProjectExpense> expenses, decimal expectedProjectEarnings, decimal agencyFeeDecimal, decimal actualProjectEarnings)
+    public static void SetRelativeExpensesAmounts(IEnumerable<ProjectExpense> expenses, IEnumerable<ProfitSharingExpense> profitSharingExpenses, decimal expectedProjectEarnings, decimal agencyFeeDecimal, decimal actualProjectEarnings)
     {
-        var totalAbsoluteExpenses = expenses.Where(x => !x.IsRelative).Select(x => x.Amount).Sum();
+        var totalAbsoluteExpenses = expenses.Sum(x => x.Amount);
         var expectedAbsoluteAgencyFee = expectedProjectEarnings * agencyFeeDecimal;
         var expectedProfitLeft = expectedProjectEarnings - (totalAbsoluteExpenses + expectedAbsoluteAgencyFee);
 
         var actualProfitLeft = actualProjectEarnings - totalAbsoluteExpenses;
 
-        foreach (var relExpense in expenses.OfType<ProfitSharingExpense>())
+        foreach (var relExpense in profitSharingExpenses)
         {
             relExpense.ExpectedAmount = expectedProfitLeft > 0 ? expectedProfitLeft * relExpense.RelativeFeeDecimal : 0;
             relExpense.Amount = actualProfitLeft > 0 ? actualProfitLeft * relExpense.RelativeFeeDecimal : 0;
